81a5492410b7610332359b69c46877d40087b9b4
Lancer <lancer123987@gmail.com>
2020-06-13 08:03:48 +0800

    0613

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    //選單背景
     $(window).scroll(function() {
         var scrollVal = $(this).scrollTop();
         if (scrollVal > 80) {
@@ -8,12 +9,14 @@ $(document).ready(function() {
             $('header').removeClass('active');
         } else {}
     });
+    //按鈕效果
     $('.bt_click').mousedown(function() {
         $(this).addClass('active');
     });
     $('.bt_click').mouseup(function() {
         $(this).removeClass('active');
     });
+    //回到頂部
     $('.goToTop').click(function() {
         var target = $(this).attr('href');
         var position = $("html, body").offset().top;
